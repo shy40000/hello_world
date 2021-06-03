2fa6ca7ea58eb6ae6a57b06b86edb821ecfd9058
shy40000 <83275490+shy40000@users.noreply.github.com>
2021-06-03 15:00:50 +0530

    Update read file.c

--- read file.c
+++ read file.c
@@ -6,20 +6,15 @@ int main()
    int num;
    FILE *fptr;
 
-   // use appropriate location if you are using MacOS or Linux
-   fptr = fopen("C:\\program.txt","w");
-
-   if(fptr == NULL)
-   {
-      printf("Error!");   
-      exit(1);             
+   if ((fptr = fopen("C:\\program.txt","r")) == NULL){
+       printf("Error! opening file");
+       exit(1);
    }
 
-   printf("Enter num: ");
-   scanf("%d",&num);
+   fscanf(fptr,"%d", &num);
 
-   fprintf(fptr,"%d",num);
-   fclose(fptr);
-
+   printf("Value of n=%d", num);
+   fclose(fptr); 
+  
    return 0;
 }
